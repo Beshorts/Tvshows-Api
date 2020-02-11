521f0440c8dae2337110df51a27c16686389be67
Beatrice Corti <beshorts@mail.com>
2020-02-11 17:08:59 +0100

    add genres params controller and partials

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,7 @@ Rails.application.routes.draw do
 
   resources :tvshows, only: [:index, :show]
   root to: 'pages#home'
+  get '/genre', to: 'pages#genre', as: 'genre'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
