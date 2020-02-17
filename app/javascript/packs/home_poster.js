190027618d62a7325b332a42b718160a9b5a22ec
Beatrice Corti <beshorts@mail.com>
2020-02-17 11:15:43 +0100

    add 3tvshow images on pages#home with javascript fetch Api

--- app/javascript/packs/home_poster.js
+++ app/javascript/packs/home_poster.js
@@ -1,11 +1,14 @@
-const = results = document.querySelector("#results");
-
-fetch("https://api.tvmaze.com/search/shows?q=watchmen")
-.then(response => response.json())
-.then((data) => {
-  data.Search.forEach((result) => {
-    const movie = `<img src="${result['show']['image']['medium']}" alt="">`;
-    results.insertAdjacentHTML("beforeend", movie);
-  });
+// add 3 tvshow images on pages#home
+const results = document.querySelector("#results");
+Promise.all([
+    fetch("https://api.tvmaze.com/singlesearch/shows?q=watchmen").then(response => response.json()),
+    fetch("https://api.tvmaze.com/singlesearch/shows?q=dare-me").then(response => response.json()),
+    fetch("https://api.tvmaze.com/singlesearch/shows?q=mandalorian").then(response => response.json()),
+]).then((data) => {
+    data.forEach((result) => {
+        const movie = `<div class"show-image >
+        <img src="${result['image']['medium']}" alt="">
+        </div>`;
+        results.insertAdjacentHTML("beforeend", movie);
+    });
 });
-
