7b4540270ac5c3c318dcc11a6135f9d510266735
Beatrice Corti <beshorts@mail.com>
2020-02-20 09:58:39 +0100

    redirect user on index page after login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,8 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
+
+  # redirect user on tvshows index page after login
+  def after_sign_in_path_for(*)
+    tvshows_path(current_user)
+  end
 end
