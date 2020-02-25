b4962f58940015f11b7e395004e93e47c6c0e794
Beatrice Corti <beshorts@mail.com>
2020-02-25 17:32:24 +0100

    done layout genres - helper for genre cards

--- app/helpers/genre_helper.rb
+++ app/helpers/genre_helper.rb
@@ -3,4 +3,15 @@ module GenreHelper
   def genre_found?
     genre_path(@genre.shows).include?(params[:genre])
   end
+
+  # to avoid tv shows who don't fit cards parameters as upcoming cards
+  def genre_filter
+    @genre.shows.select do |item|
+      unless item['show']['genres'].empty? || item['show']['genres'].nil?
+        unless item['show']['genres'][0] == 'DIY' || item['show']['genres'][0] == 'Adult'
+          item
+        end
+      end
+    end
+  end
 end
